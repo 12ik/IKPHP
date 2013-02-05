bc118da5cb460e5388dcbd1e068c214c80c7c7e8
12ik <12ik.wang@gmail.com>
2013-02-05 20:46:03 +0800

    Signed-off-by: 12ik <12ik.wang@gmail.com>

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/* 当前IKPHP程序版本 */
+define('IK_VERSION', '1.0');
+/* 当前IKPHP程序Release */
+define('IK_RELEASE', '20130201');
+/* 应用名称*/
+define('APP_NAME', 'app');
+/* 应用目录*/
+define('APP_PATH', './app/');
+/* 数据目录*/
+define('PIN_DATA_PATH', './data/');
+
+require './core/ThinkPHP.php';
